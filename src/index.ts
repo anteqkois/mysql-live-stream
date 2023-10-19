1cd7f303217bb2cdd56caf84e2d1a217cc861e0c
anteqkois <anteqkois.dev@gmail.com>
2023-10-19 19:48:39 +0200

    (logs) working filter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-import ZongJi from 'zongji'
+import "dotenv/config";
+import mysql from "mysql2";
+import ZongJi from "zongji";
 
-let watcher = new ZongJi({
+const connection = mysql.createConnection({
+  port: process.env.DATABASE_PORT as unknown as number,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 
- });
+connection.connect();
+
+// @ts-ignore
+let watcher = new ZongJi(connection);
 
 // Each change to the replication log results in an event
-zongji.on('binlog', function(evt) {
-  evt.dump();
+watcher.on("binlog", function (event) {
+  if (event.getEventName() === "writerows")
+    console.dir({
+      timestamp: event.timestamp,
+      rows: event.rows,
+      tableName: event.tableMap[event.tableId].tableName,
+    });
 });
 
 // Binlog must be started, optionally pass in filters
-zongji.start({
-  includeEvents: ['tablemap', 'writerows', 'updaterows', 'deleterows']
+watcher.start({
+  startAtEnd: true,
+  includeSchema: {
+    "coins-price": ["btcusd"],
+  },
+  includeEvents: ["tablemap", "writerows"],
 });
